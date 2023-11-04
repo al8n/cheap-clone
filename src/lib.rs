c39aac7922b223918fefa6387a9c8d6f7c7953db
Al Liu <scygliu1@gmail.com>
2023-11-05 00:52:27 +0800

    Add tuple support

--- src/lib.rs
+++ src/lib.rs
@@ -83,6 +83,7 @@ impl<T: CheapClone, E: CheapClone> CheapClone for Result<T, E> {}
 impl<L: CheapClone, R: CheapClone> CheapClone for either::Either<L, R> {}
 
 impl_cheap_clone_for_copy! {
+  (),
   bool, char, f32, f64, i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize,
   core::num::NonZeroI8,
   core::num::NonZeroI16,
@@ -104,3 +105,66 @@ impl<T: Copy, const N: usize> CheapClone for [T; N] {
     *self
   }
 }
+
+impl<'a, T> CheapClone for &'a T {
+  fn cheap_clone(&self) -> Self {
+    self
+  }
+}
+
+macro_rules! impl_cheap_clone_for_tuple {
+  ($($param:literal),+ $(,)?) => {
+    paste::paste! {
+      impl<$([< T $param >]: CheapClone),+> CheapClone for ($([< T $param >],)+) {
+        fn cheap_clone(&self) -> Self {
+          ($(self.$param.cheap_clone(),)+)
+        }  
+      }
+    }
+  };
+}
+
+impl_cheap_clone_for_tuple!(0);
+impl_cheap_clone_for_tuple!(0, 1);
+impl_cheap_clone_for_tuple!(0, 1, 2);
+impl_cheap_clone_for_tuple!(0, 1, 2, 3);
+impl_cheap_clone_for_tuple!(0, 1, 2, 3, 4);
+impl_cheap_clone_for_tuple!(0, 1, 2, 3, 4, 5);
+impl_cheap_clone_for_tuple!(0, 1, 2, 3, 4, 5, 6);
+impl_cheap_clone_for_tuple!(0, 1, 2, 3, 4, 5, 6, 7);
+impl_cheap_clone_for_tuple!(0, 1, 2, 3, 4, 5, 6, 7, 8);
+impl_cheap_clone_for_tuple!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+impl_cheap_clone_for_tuple!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+impl_cheap_clone_for_tuple!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+impl_cheap_clone_for_tuple!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+impl_cheap_clone_for_tuple!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+impl_cheap_clone_for_tuple!(
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+);
+impl_cheap_clone_for_tuple!(
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15
+);
+impl_cheap_clone_for_tuple!(
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16
+);
+impl_cheap_clone_for_tuple!(
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16, 17
+);
+impl_cheap_clone_for_tuple!(
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16, 17, 18
+);
+impl_cheap_clone_for_tuple!(
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16, 17, 18, 19
+);
+impl_cheap_clone_for_tuple!(
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16, 17, 18, 19, 20
+);
+impl_cheap_clone_for_tuple!(
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16, 17, 18, 19, 20, 21
+);
+impl_cheap_clone_for_tuple!(
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16, 17, 18, 19, 20, 21, 22
+);
+impl_cheap_clone_for_tuple!(
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16, 17, 18, 19, 20, 21, 22, 23
+);
